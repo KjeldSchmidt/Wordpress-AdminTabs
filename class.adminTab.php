c6fdb18f94099de243edbed929c6444b36e56537
Kjeld Schmidt <superfluidmercury@live.de>
2014-08-20 23:00:17 +0200

    First working draft of tab class
    Tab class should now properly assign name, slug and filename to itself.

--- class.adminTab.php
+++ class.adminTab.php
@@ -32,12 +32,42 @@ class Tab {
 	
 	public $name;
 	public $slug;
-	public $filename;
+	public $file;
 
 
-	function __construct($tabs) {
+	function __construct($tabBasis) {
+		if(is_array($tabBasis)) {
+			if (isset($tabBasis[0]))	$this->name = $tabBasis[0];
+			if (isset($tabBasis[1]))	$this->slug = $tabBasis[1];
+			if (isset($tabBasis[2]))	$this->file = $tabBasis[2];
 
+			if(!isset($this->slug))		guessFromName($this->name);
+
+			if(!isset($this->file))		$this->file = $this->slug;
+
+		} else guessFromName($tabBasis);
+
+		$this->file .= '.php';
 	}
+
+	function guessFromName($name) {
+		$this->name	= $name;
+		$this->slug	= makeSlug($name);
+		$this->file = $this->slug;
+	}
+
+	function makeSlug($string) {
+		$text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+		$text = trim($text, '-');
+		$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+		$text = strtolower($text);
+		$text = preg_replace('~[^-\w]+~', '', $text);
+
+		if (empty($text)) {	return 'n-a'; }
+
+  		return $text;
+	}
+	
 }
 
 ?>
